d7fb9aaec64643ae288693759f47473691c7b4b5
SeungWook0502 <SeungWook0502@gmail.com>
2023-05-02 17:06:56 +0900

    feat: Add input data split
    Split input_data to epoch and type.

--- mt_input.py
+++ mt_input.py
@@ -1,4 +1,8 @@
 class MtInput:
     def mt_input():
-        print("ex) 4/keep or 100/change")
-        input_data = input("Enter the count and target: ")
+        while True:
+            print("ex) 4/keep or 100/change")
+            input_data = input("Enter the count and target: ")
+            input_list = input_data.split('/')
+            epoch = int(input_list[0])
+            type = input_list[1]
